0ebecf76d3a24a5230679d64f292573ae7d2ebd9
Viktar Yafimchyk <viktar.yafimchyk@carsales.com.au>
2015-04-16 10:55:46 +1000

    Test last change Date retrieval logic.

--- grails-app/controllers/edu/test/many/to/many/LiveStockController.groovy
+++ grails-app/controllers/edu/test/many/to/many/LiveStockController.groovy
@@ -14,4 +14,11 @@ class LiveStockController {
         }
         render (result as JSON)
     }
+
+    def lastChangeDate() {
+        println params.id
+        def priceHistories = LiveStock.get(params.id).priceHistories
+        def lastPriceChangeDate = priceHistories.size() > 1 ? priceHistories.max {it.changeDate}.changeDate : null
+        render ([lastPriceChangeDate] as JSON)
+    }
 }
