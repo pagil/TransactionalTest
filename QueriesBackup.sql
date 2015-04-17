c5c11e78e48fa7442583deacef8b00484acf3b85
Viktar Yafimchyk <viktar.yafimchyk@carsales.com.au>
2015-04-17 11:01:05 +1000

    Update SQL scripts.

--- QueriesBackup.sql
+++ QueriesBackup.sql
@@ -9,3 +9,10 @@ select * from (select ls.ID NEW_ID, count(*) NEW_COUNT from LIVE_STOCK ls
   join PRICE_HISTORY ph on lsph.PRICE_HISTORY_ID = ph.ID
 group by ls.ID)
 where NEW_COUNT> 1;
+
+select MAX(ph.CHANGE_DATE) from PRICE_HISTORY ph where ph.ID in (select lsph.PRICE_HISTORY_ID from LIVE_STOCK_PRICE_HISTORY lsph where lsph.LIVE_STOCK_PRICE_HISTORIES_ID = 1);
+
+select
+  *,
+  (select MAX(ph.CHANGE_DATE) from PRICE_HISTORY ph where ph.ID in (select lsph.PRICE_HISTORY_ID from LIVE_STOCK_PRICE_HISTORY lsph where lsph.LIVE_STOCK_PRICE_HISTORIES_ID = ls.ID))
+from LIVE_STOCK ls;
